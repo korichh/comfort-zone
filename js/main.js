313ef04dfb9c2a6f24c6864e626babf8dfefe3f0
korGetAv <korichhak@gmail.com>
2024-03-15 15:44:22 +0200

    apartment

--- js/main.js
+++ js/main.js
@@ -54,10 +54,20 @@ const main = function () {
         const officeSwiperSelectors = apartment.querySelectorAll('.office-list .swiper')
 
         const thumbs = new Swiper(thumbsSelector, {
-            spaceBetween: 25,
-            slidesPerView: 4,
+            spaceBetween: 15,
+            slidesPerView: 2,
             freeMode: true,
             watchSlidesProgress: true,
+            breakpoints: {
+                499.98: {
+                    slidesPerView: 3,
+                    spaceBetween: 15,
+                },
+                767.98: {
+                    slidesPerView: 4,
+                    spaceBetween: 25,
+                }
+            }
         })
 
         const swiper = new Swiper(swiperSelector, {
