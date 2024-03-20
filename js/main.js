cf682a301ab7c6e02bf275c63a38d0b6a9cfa90a
korGetAv <korichhak@gmail.com>
2024-03-20 17:34:08 +0200

    action

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 const main = function () {
     const goTop = document.querySelector('.go-top');
+    const header = document.querySelector('.header');
     const mobile = document.querySelector('.mobile');
     const popup = document.querySelector('.popup');
     const office = document.querySelector('.office');
+    const filter = document.querySelector('.office-filter');
     const apartment = document.querySelector('.apartment');
     const calendar = document.querySelector('.calendar');
 
@@ -19,6 +21,25 @@ const main = function () {
 
         document.addEventListener('scroll', checkScroll);
         checkScroll();
+    }
+
+    if (header) {
+        const profile = document.querySelector('.action.profile')
+
+        header.addEventListener('click', (e) => {
+            if (e.target.closest('[data-profile]')) {
+                e.preventDefault()
+                profile.classList.add('_active')
+                document.body.classList.add('_lock')
+            }
+        })
+
+        profile.addEventListener('click', (e) => {
+            if (profile.classList.contains('_active') && (!e.target.closest('.profile .action-inner') || e.target.closest('.profile .close'))) {
+                profile.classList.remove('_active')
+                document.body.classList.remove('_lock')
+            }
+        })
     }
 
     if (mobile) {
@@ -55,6 +76,7 @@ const main = function () {
                 document.body.classList.add('_lock')
             }
         })
+
         popup.addEventListener('click', (e) => {
             if (popup.classList.contains('_active') && (!e.target.closest('.popup-inner') || e.target.closest('.popup .close'))) {
                 popup.classList.remove('_active')
@@ -65,14 +87,19 @@ const main = function () {
 
     if (office) {
         const swiperSelectors = office.querySelectorAll('.office-list .swiper')
-        const filter = office.querySelector('.office-filter')
 
         initOfficeSwiper(swiperSelectors)
+    }
 
-        if (filter) {
-            filter.addEventListener('click', (e) => { if (e.target.closest('.toggle')) filter.classList.toggle('_active') })
-            document.addEventListener('click', (e) => { if (!e.target.closest('.office-filter')) filter.classList.remove('_active') })
-        }
+    if (filter) {
+        const dateInput = filter.querySelector('#date')
+        const timeInput = filter.querySelector('#time')
+
+        initPicker(dateInput)
+        initPicker(timeInput, true)
+
+        filter.addEventListener('click', (e) => { if (e.target.closest('.toggle')) filter.classList.toggle('_active') })
+        document.addEventListener('click', (e) => { if (!e.target.closest('.office-filter') && !e.target.closest('.flatpickr-calendar')) filter.classList.remove('_active') })
     }
 
     if (apartment) {
@@ -112,39 +139,35 @@ const main = function () {
 
     if (calendar) {
         const swiperSelector = calendar.querySelector('.calendar-swiper .swiper')
-        const swiperNext = swiperSelector.querySelector('.swiper-button-next')
-        const swiperPrev = swiperSelector.querySelector('.swiper-button-prev')
         const wWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        const dateInput = calendar.querySelector('#date')
+        const booking = document.querySelector('.action.booking')
 
-        const swiper = new Swiper(swiperSelector, {
-            navigation: {
-                nextEl: swiperNext,
-                prevEl: swiperPrev,
-            },
-            enabled: true,
-            spaceBetween: 10,
-            slidesPerView: 1,
-            breakpoints: {
-                991.98: {
-                    slidesPerView: 2,
-                    spaceBetween: 15,
-                }
-            }
-        })
+        initCalendarSwiper(swiperSelector)
+        initPicker(dateInput)
 
         calendar.addEventListener('click', (e) => {
             if (e.target.closest('.switch button')) {
                 const btns = e.target.closest('.switch').querySelectorAll('button')
                 for (const btn of btns) btn.classList.remove('_active')
                 e.target.closest('.switch button').classList.add('_active')
+            } else if (e.target.closest('.grid button:not([data-disabled])')) {
+                booking.classList.add('_active')
+                document.body.classList.add('_lock')
+            }
+        })
+
+        booking.addEventListener('click', (e) => {
+            if (booking.classList.contains('_active') && (!e.target.closest('.booking .action-inner') || e.target.closest('.booking .close') || e.target.closest('.booking button[type="reset"]'))) {
+                booking.classList.remove('_active')
+                document.body.classList.remove('_lock')
             }
         })
 
         if (wWidth > 992) {
-            const calendarSwiper = calendar.querySelector('.calendar-swiper')
             const tooltip = calendar.querySelector('.tooltip')
 
-            calendarSwiper.addEventListener('mouseover', (e) => {
+            calendar.addEventListener('mouseover', (e) => {
                 if (e.target.closest('.grid span') || e.target.closest('.grid button[data-disabled]')) {
                     const target = e.target.closest('.grid span') || e.target.closest('.grid button[data-disabled]')
                     const top = target.getBoundingClientRect().top + target.offsetHeight
@@ -157,7 +180,7 @@ const main = function () {
                 }
             })
 
-            calendarSwiper.addEventListener('mouseout', (e) => {
+            calendar.addEventListener('mouseout', (e) => {
                 if (e.target.closest('.grid span') || e.target.closest('.grid button[data-disabled]')) {
                     tooltip.classList.remove('_active')
                 }
@@ -183,5 +206,44 @@ const main = function () {
                 }
             })
         }
+    }
+
+    function initCalendarSwiper(selector) {
+        const next = selector.querySelector('.swiper-button-next')
+        const prev = selector.querySelector('.swiper-button-prev')
+
+        const swiper = new Swiper(selector, {
+            navigation: {
+                nextEl: next,
+                prevEl: prev,
+            },
+            enabled: true,
+            spaceBetween: 10,
+            slidesPerView: 1,
+            breakpoints: {
+                991.98: {
+                    slidesPerView: 2,
+                    spaceBetween: 15,
+                }
+            }
+        })
+    }
+
+    function initPicker(elem, isTime = false) {
+        if (isTime) {
+            return flatpickr(elem, {
+                enableTime: true,
+                noCalendar: true,
+                locale: elem.getAttribute('data-locale'),
+                defaultDate: new Date(),
+            })
+        }
+        return flatpickr(elem, {
+            monthSelectorType: 'static',
+            locale: elem.getAttribute('data-locale'),
+            defaultDate: new Date(),
+            minDate: new Date(),
+            maxDate: new Date(new Date().setMonth(new Date().getMonth() + 6)),
+        })
     }
 }();
